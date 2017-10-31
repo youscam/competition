31417caa51f770a7f1933279cd588a6b25e157a3
kamal_azzouzi <youscam@outlook.com>
2017-10-31 21:44:09 +0000

    First commit

---
+++ src/main/java/ma/youscam/competition/CompetitionApplication.java
@@ -0,0 +1,12 @@
+package ma.youscam.competition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetitionApplication.class, args);
+	}
+}
