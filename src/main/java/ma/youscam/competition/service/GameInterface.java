564ae0069d1be192dded26554e8b3d8ccb4e0714
kamal_azzouzi <youscam@outlook.com>
2017-11-07 19:41:18 +0000

    Add business logic

---
+++ src/main/java/ma/youscam/competition/service/GameInterface.java
@@ -0,0 +1,7 @@
+package ma.youscam.competition.service;
+
+public interface GameInterface {
+
+	ResultClubsCompetition getResultClubs();
+	ResultClubsCompetition getResultClubs(Long CourseId);
+}
--- src/main/java/ma/youscam/competition/service/ResultClub.java
+++ src/main/java/ma/youscam/competition/service/ResultClub.java
@@ -4,19 +4,20 @@ import java.util.Set;
 
 import ma.youscam.competition.model.Club;
 import ma.youscam.competition.model.Player;
+import ma.youscam.competition.model.Secore;
 
-public class ResultClub {
+public class ResultClub implements Comparable<ResultClub>{
 	private Club club;
 	private int position;
 	private int note;
 	private Set<Player> joueursgagnes;
 	
-	public ResultClub(Club club, int position, int note, Set<Player> joueursgagnes) {
+	public ResultClub(Club club, Set<Secore> secores) {
 		super();
 		this.club = club;
-		this.position = position;
-		this.note = note;
-		this.joueursgagnes = joueursgagnes;
+//		this.position = position;
+//		this.note = note;
+//		this.joueursgagnes = joueursgagnes;
 	}
 
 	public Club getClub() {
@@ -49,6 +50,13 @@ public class ResultClub {
 
 	public void setJoueursgagnes(Set<Player> joueursgagnes) {
 		this.joueursgagnes = joueursgagnes;
+	}
+
+	@Override
+	public int compareTo(ResultClub o) {
+		if(this.getNote()>o.getNote()) return -1;
+		if(this.getNote()<o.getNote()) return  1;
+		return 0;
 	}
 	
 	
